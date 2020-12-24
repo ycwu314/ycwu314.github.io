ea2cfdfb3922d532770b0e6410d3c8aaf1b40ca7
ycwu314 <535215779@qq.com>
2020-12-24 16:29:34 +0800

    Site updated: 2020-12-24 16:29:33

---
+++ js/schemes/muse.js
@@ -0,0 +1,3 @@
+// build time:Thu Dec 24 2020 16:29:30 GMT+0800 (China Standard Time)
+$(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
+//rebuild by neat 
